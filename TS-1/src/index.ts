732ea7dadf94cc9e24e0ffbdce3bd82ed6cf5948
Mayank <mayankkumardas12@gmail.com>
2025-03-19 17:19:08 +0530

    first Commit

---
+++ TS-1/src/index.ts
@@ -0,0 +1,65 @@
+/* 
+    let a: number = 1;
+    console.log(a);
+*/
+
+// greet("Mayank")
+
+// function greet (firstname:string){
+
+//     console.log("Hello"+ firstname)
+
+// }
+
+// let z:any = 1
+
+// z = "Mayank"
+// z = true
+
+// console.log(z);
+
+// function Sum(a: number, b: number): number {
+//     return a + b;
+// }
+
+// let ans = Sum(3, 4)
+// console.log(ans);
+
+
+/*
+    function delayedCall(fxn: () => void) {
+        setTimeout(fxn, 1000);
+    }
+
+    delayedCall(() => {
+        console.log("Hello World");
+    })
+*/
+
+/*
+function delayedCall(fxn: () => number) {
+    setTimeout(fxn, 1000);
+}
+
+delayedCall(() => {
+    console.log("Hello World");
+    return 1;
+})
+
+*/
+
+let user = {
+    name: "Mayank",
+    age: 21,
+}
+
+function greet(user: {
+    name: string,
+    age: number,
+}) {
+
+    console.log("Hello " + user.age);
+    
+}
+
+greet(user)
